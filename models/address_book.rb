36365a7935eda339fc98504eb012b7f307391324
Ashton N. Levier <anlevier@gmail.com>
2015-08-26 16:33:27 -0600

    remove entry

--- models/address_book.rb
+++ models/address_book.rb
@@ -9,16 +9,20 @@ class AddressBook
    end
    
    def add_entry(name, phone, email)
- # #9
+
      index = 0
      @entries.each do |entry|
- # #10
+
        if name < entry.name
          break
        end
        index += 1
      end
- # #11
+ 
      @entries.insert(index, Entry.new(name, phone, email))
+   end
+
+   def remove_entry(name, phone, email)
+    
    end
 end
